284cfe1bfac11de7002d15c306554f158cdaeea8
OVMako <58699743+OVMako@users.noreply.github.com>
2020-03-04 16:26:02 +0300

    2

--- dz1/js/main.js
+++ dz1/js/main.js
@@ -5,14 +5,14 @@ const products = [
     {id: 4, title: 'Gamepad', price: 50, img: 'https://placehold.it/200x150'},
 ];
 
-const renderProduct = (title, price) => {
-    return `<div class="product-item">
-                <img src="https://placehold.it/200x150" alt="${title}">
-                <h3>${title}</h3>
-                <p>${price}$</p>
-                <button class="buy-btn">Купить</button>
-            </div>`
-};
+const renderProduct = (title, price) =>
+     `<div class="product-item">
+        <img src="https://placehold.it/200x150" alt="${title}">
+        <h3>${title}</h3>
+        <p>${price}$</p>
+        <button class="buy-btn">Купить</button>
+    </div>`
+
 const renderPage = list => {
     const productsList = list.map(item => renderProduct(item.title, item.price));
     document.querySelector('.products').innerHTML = productsList;
