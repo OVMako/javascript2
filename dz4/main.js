419329311a2a80b50c730a7ce08e3b94d2d79f00
OVMako <58699743+OVMako@users.noreply.github.com>
2020-03-14 19:54:12 +0300

    №1-2
    1. Дан большой текст, в котором для оформления прямой речи используются одинарные кавычки. Придумать шаблон, который заменяет одинарные кавычки на двойные.
    2. Улучшить шаблон так, чтобы в конструкциях типа aren't одинарная кавычка не заменялась на двойную.

--- dz4/main.js
+++ dz4/main.js
@@ -3,9 +3,7 @@ const inputs = document.querySelectorAll('input')
 const patterns = {
 	telephone: /^\d{10}$/,
 	username: /^[A-ZА-Я]{1}[a-zа-я]*$/,
-	password: /^[\w@-]{8,20}$/,
-	slug: /^[a-z\d-]{8,20}$/,
-	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+	email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,5})$/
 };
 
 function validate(field, regex) {
@@ -22,4 +20,10 @@ inputs.forEach((input) => {
 		//console.log(e.target.attributes.name.value);
 		validate(e.target, patterns[e.target.attributes.name.value]);
 	})
-})		
+})
+
+const a = "  One: 'HowOne: 'Hi Mary.' Two: 'Oh, hi.' are you doing?' Two: 'I'm doing alright. How about you?' One: 'Not too bad. The weather is great isn't it?' Two: 'Yes. It's absolutely beautiful today.' One: 'I wish it was like this more frequently.' Two: 'Me too.' One: 'So where are you going now?' Two: 'I'm going to meet a friend of mine at the department store.' One: 'Going to do a little shopping?' Two: 'Yeah, I have to buy some presents for my parents.' One: 'What's the occasion?' Two: 'It's their anniversary.' One: 'That's great. Well, you better get going. You don't want to be late.' Two: 'I'll see you next time.' One: 'Sure. Bye.'"
+
+const newText = a.replace (/[\s]'|'[\s]/gi,'"')
+console.log(newText)
+
